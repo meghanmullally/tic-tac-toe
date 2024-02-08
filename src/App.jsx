1c563e2f303acb0375e1750dd415a510a024cf57
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-08 15:47:01 -0800

    create Player component and set up inital player 1 and player 2 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import Player from "./components/Player";
+
 
 function App() {
-  
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name="Player 1" symbol="X"/>
+        <Player name="Player 2" symbol="O"/>
+      </ol>
 
-  return (
-    <p>Coming soon...</p>
-  )
+      GAME BOARD
+    </div>
+
+    LOG
+  </main>;
 }
 
 export default App
