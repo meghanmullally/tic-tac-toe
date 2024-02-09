41657829768ded38602afe1f8382bed914cb1d37
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-08 19:52:28 -0800

    fix formatting

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,24 +9,24 @@ export default function Player({ initName, symbol }) {
         // another option would of been a turnary setIsEditing(isEditing ? false : true); 
         // setIsEditing(!isEditing); // => true
         // best option would be passing a value to return the new state you want to set - always using with the available value from state updating function
-        setIsEditing((editing)=> !editing); // => gets latest state value from react, at point at time where it is executed
+        setIsEditing((editing) => !editing); // => gets latest state value from react, at point at time where it is executed
     }
-    
+
     function handleChange(event) {
-    // hold the value the user tried to enter
+        // hold the value the user tried to enter
         setCurrentPlayerName(event.target.value);
     }
-    
+
     let editablePlayerName = <span className="player-name">{currentPlayerName}</span>;
 
     // if isEditing is true - then we will have an input field instead
-    if(isEditing) {
-        editablePlayerName = <input type="text" required value={currentPlayerName} onChange={handleChange}/>
-    } 
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={currentPlayerName} onChange={handleChange} />
+    }
 
 
 
-    
+
     return (
         <li>
             <span className="player">
