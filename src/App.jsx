6ff7024706633928bf77d4de0597860c4c7713aa
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-08 16:34:19 -0800

    update name to be initName as we will need to be able to edit the name later, adding notes from react video, initial handleChange for when we want to save the currentPlayers name

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
   return <main>
     <div id="game-container">
       <ol id="players">
-        <Player name="Player 1" symbol="X"/>
-        <Player name="Player 2" symbol="O"/>
+{/* components instatnces work in isolation - when clicking on edit, it edits 1 player at a time, because it creates a separate instance for each */}
+        <Player initName="Player 1" symbol="X"/>
+        <Player initName="Player 2" symbol="O"/>
       </ol>
 
       GAME BOARD
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,29 +1,38 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initName, symbol }) {
 
+    const [currentPlayer, setCurrentPlayer] = useState(initName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(true);
+        // another option would of been a turnary setIsEditing(isEditing ? false : true); 
+        // setIsEditing(!isEditing); // => true
+        // best option would be passing a value to return the new state you want to set - always using with the available value from state updating function
+        setIsEditing((editing)=> !editing); // => gets latest state value from react, at point at time where it is executed
     }
-
-    let playerName = <span className="player-name">{name}</span>;
+    
+    function handleChange(event) {
+        setCurrentPlayer();
+    }
+    
+    let editablePlayerName = <span className="player-name">{currentPlayer}</span>;
 
     // if isEditing is true - then we will have an input field instead
     if(isEditing) {
-        playerName = <input type="text" required />
+        editablePlayerName = <input type="text" required value={currentPlayer} onChange={handleChange}/>
     } 
+
 
 
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
