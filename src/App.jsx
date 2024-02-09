9e5cc244e2fe0ad07b5ca6070232c170cbcfd616
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-08 19:52:12 -0800

    create GameBoard componenet and add to app.js, create board layout with row and colums

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 
 function App() {
   return <main>
     <div id="game-container">
       <ol id="players">
-{/* components instatnces work in isolation - when clicking on edit, it edits 1 player at a time, because it creates a separate instance for each */}
-        <Player initName="Player 1" symbol="X"/>
-        <Player initName="Player 2" symbol="O"/>
+        {/* components instatnces work in isolation - when clicking on edit, it edits 1 player at a time, because it creates a separate instance for each */}
+        <Player initName="Player 1" symbol="X" />
+        <Player initName="Player 2" symbol="O" />
       </ol>
 
-      GAME BOARD
+      <GameBoard />
     </div>
 
     LOG
