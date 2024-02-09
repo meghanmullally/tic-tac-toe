80a29248f7b683211099bb547c0b563a942cdf2a
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-08 16:01:51 -0800

    useState for storing data about whether user isEditing the player name, use a turnary if edit show input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(true);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+
+    // if isEditing is true - then we will have an input field instead
+    if(isEditing) {
+        playerName = <input type="text" required />
+    } 
+
+
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     );
 }
