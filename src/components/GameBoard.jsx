67d9484f09aeb493496e9e40790be08cca2cd7d0
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-12 14:36:20 -0800

    removed commeneted out code and move variables to app.js to clean up code and props

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,41 +1,7 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-
-    // computed value that is dervied from state 
-    // deriving our gameboard from turns
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        // update row and col with the player symbol - this is a dervied State.
-        gameBoard[row][col] = player;
-    }
-
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // // handle which row and colum and symbol was selected
-    // function handleSelect(rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) => {
-
-    //         // updated the state in a immuatable (unchanging over time or unable to be changed) way 
-    //         const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedGameBoard;
-    //     });
-
-    //     onSelectSquare();
-    // }
+export default function GameBoard({ onSelectSquare, board }) {
 
     return <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => (
+        {board.map((row, rowIndex) => (
             <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
