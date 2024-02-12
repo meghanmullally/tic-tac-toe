f56c444282430668aefa8d9997441b70bbbb6c70
Meghan Mullally <ms.mullally27@gmail.com>
2024-02-12 14:37:11 -0800

    add GameBoard variables, work on winning combos and add GameOver text

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import GameOver from "./components/GameOver";
+import { WINNING_COMBINATIONS } from "./components/winning-combinations";
 
 
-// create a helper function 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
@@ -20,6 +27,37 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    // update row and col with the player symbol - this is a dervied State.
+    gameBoard[row][col] = player;
+  }
+
+let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    // access row in gameboard, then want to access col in gameboard
+    // different winning combos
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && 
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol) {
+        winner = firstSquareSymbol;
+    } 
+
+  }
+
+  // has a draw if no winner or the game lenght is 9
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+
   function handleSelectSquare(rowIndex, colIndex) {
 
     // want to log whos turn it was 
@@ -27,34 +65,37 @@ function App() {
       let currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer },
-      ...prevTurns
+      ...prevTurns,
       ];
 
       return updatedTurns;
     });
   }
 
-  return <main>
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        {/* components instances work in isolation - when clicking on edit, it edits 1 player at a time, because it creates a separate instance for each */}
-        {/* player 1 is active is X and player 2 is active if O */}
-        <Player
-          initName="Player 1"
-          symbol="X"
-          isActive={activePlayer === 'X'} />
-        <Player
-          initName="Player 2"
-          symbol="O"
-          isActive={activePlayer === 'O'} />
-      </ol>
-      <GameBoard
-        onSelectSquare={handleSelectSquare}
-        turns={gameTurns}
-      />
-    </div>
-    <Log turns={gameTurns} />
-  </main>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          {/* components instances work in isolation - when clicking on edit, it edits 1 player at a time, because it creates a separate instance for each */}
+          {/* player 1 is active is X and player 2 is active if O */}
+          <Player
+            initName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'} />
+          <Player
+            initName="Player 2"
+            symbol="O"
+            isActive={activePlayer === 'O'} />
+        </ol>
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          board={gameBoard}
+        />
+      </div>
+      <Log turns={gameTurns} />
+    </main>
+  );
 }
 
 export default App
